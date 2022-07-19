e588d5ca77e7928a207b4f99d88c34f51babd9a9
nana8901@gmail.com <nathan@Nathans-MacBook-Pro.local>
2022-07-19 15:06:45 -0400

    adding some stuff to check if the Item class is functioning

--- store_item.rb
+++ store_item.rb
@@ -14,3 +14,22 @@ item3 = {
   color: "black",
   price: 54.99
 }
+p "The #{item1[:name]} is #{item1[:color]}, and is $#{item1[:price]}"
+
+class Item
+  def initialize(name, color, price)
+    @name = name
+    p @name
+    @color = color
+    p @color
+    @price = price
+    p @price
+  end
+
+  def print_info
+    p "#{@name} is #{@color} and costs #{@price}"
+  end
+end
+
+item1 = Item.new("bike", "green", 46.99)
+item1.print_info
