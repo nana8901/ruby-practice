af0899618250b008ac1b2f0d219bc58003199ca8
Nathan Brownstein <nathan@Nathans-MacBook-Pro.local>
2022-07-19 14:34:59 -0400

    This is the initial commit of an item program

---
+++ store_item.rb
@@ -0,0 +1,16 @@
+item1 = {
+  :name => "band shirt",
+  :color => "red",
+  :price => 8.99
+}
+
+item2 = {
+  :name => "khaki pants",
+  :color => "khaki",
+  :price => 14.99
+}
+item3 = {
+  name: "leather jacket",
+  color: "black",
+  price: 54.99
+}
